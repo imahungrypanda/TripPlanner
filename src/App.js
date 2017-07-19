7126172844df8b5d1899369755cb6c0560b69019
Steve Olsen <stevelolsen@gmail.com>
2017-07-18 17:06:17 -0700

    Render Map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    console.log(google.maps);
+    const mapDOMNode = ReactDOM.findDOMNode(this.refs.map);
+    console.log(mapDOMNode);
+    let mapOptions = {
+      center: {lat: 37.7749, lng: -122.4149},
+      zoom: 12
+    };
+    this.map = new google.maps.Map(mapDOMNode, mapOptions);
+    console.log(this.map);
+  }
+
   render() {
+    const mapStyle = {
+      width: 500,
+      height: 300,
+      border: '1px solid black'
+    };
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='Map'>
+        <div ref='map' id="map" style={mapStyle}>Map</div>
       </div>
     );
   }
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"],
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
