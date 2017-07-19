10f20b288bf470d5c1563310e0462a1488e564fc
Steve Olsen <stevelolsen@gmail.com>
2017-07-19 15:04:45 -0700

    Start of adding buttons

---
+++ src/buttons/ButtonContainer.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import Button from './Button';
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Button)
