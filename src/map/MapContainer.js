abb9c728a7f97e2777ec40a624b836cf54d22a2d
Steve Olsen <stevelolsen@gmail.com>
2017-07-22 10:54:17 -0700

    Clean MapContainer

--- src/map/MapContainer.js
+++ src/map/MapContainer.js
@@ -1,18 +1,13 @@
 import { connect } from 'react-redux';
 import Map from './Map';
 import { findLocation, setMarkers, addMarker, addHistory, clearMarkers } from './MapActions';
-// import { parseMarkers, parseHistoryName } from './MapUtil';
 
 const mapStateToProps = state => ({
   coords: state.map.coords
-  // markers: state.map.markers,
-  // nodes: parseMarkers(state.map.markers),
-  // history: state.map.history
 });
 
 const mapDispatchToProps = dispatch => ({
   findLocation: () => dispatch(findLocation()),
-
   addMarker: coords => dispatch(addMarker(coords))
 });
 
@@ -21,8 +16,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Map);
-
-
-  // addHistory: history => dispatch(addHistory(history)),
-  // clearMarkers: () => dispatch(clearMarkers()),
-  // getHistoryName: legs => parseHistoryName(legs)
